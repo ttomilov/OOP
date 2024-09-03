c716a4710a422f853ee9f40703f3e1e9b1734056
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-09-04 00:02:01 +0700

    a

--- Task_1_1_1/src/test/java/org/example/Test1.java
+++ Task_1_1_1/src/test/java/org/example/Test1.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Test1 {
+    /**
+     * Test1
+     */
 
     @Test
     void main() {
         int[] arr = {5, 4, 3, 2, 1};
         Heap.heapsort(arr);
-        int[] true_arr = {1, 2, 3, 4, 5};
-        for (int i = 0; i < true_arr.length; i++) {
-            assertEquals(true_arr[i], arr[i]);
+        int[] trueArr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < trueArr.length; i++) {
+            assertEquals(trueArr[i], arr[i]);
         }
     }
 }
