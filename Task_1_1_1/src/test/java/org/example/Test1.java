cb6e050362f43d24e78eaad5f0e82d6215420541
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-09-04 10:15:28 +0700

    Task_1_1_1

--- Task_1_1_1/src/test/java/org/example/Test1.java
+++ Task_1_1_1/src/test/java/org/example/Test1.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class test1 {
+class Test1 {
     /**
      * Test1.
      */
--- Task_1_1_1/src/test/java/org/example/Test2.java
+++ Task_1_1_1/src/test/java/org/example/Test2.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class test2 {
+class Test2 {
     /**
      * Test2.
      */
