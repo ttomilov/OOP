a72696372902039f76041067e6fa0ec231e20db8
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-09-16 15:59:35 +0700

    Task_1_1_2

---
+++ Task_1_1_2/src/main/java/org/example/Cards.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Cards {
+    public String nameCard;
+    public int points;
+    public char flUse;
+
+    public Cards(String name, int point, char fluse){
+        this.nameCard = name;
+        this.points = point;
+        this.flUse = fluse;
+    }
+}
---
+++ Task_1_1_2/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+import static org.example.BlackJack.game;
+import static org.example.BlackJack.makeDeck;
+
+public class Main {
+    public static void main(String[] args) {
+        Cards[] cards = new Cards[53];
+        makeDeck(cards);
+        int round = 1;
+        while (true){
+            game(cards, round++);
+            System.out.printf("Хотите завершить игру?[Y/N]: ");
+            Scanner scanner = new Scanner(System.in);
+            String fl = scanner.nextLine();
+            if (fl.equals("Y") || fl.equals("y") || fl.equals("Д") || fl.equals("д")) break;
+        }
+    }
+}
