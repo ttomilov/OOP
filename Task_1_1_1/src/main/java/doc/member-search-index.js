6e2140c67bb7dbbc3e96ade23373dafc02e41af4
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-11-09 02:08:07 +0700

    Task_1_1_3

--- Task_1_1_1/src/main/java/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.example","c":"Heap","l":"Heap()","u":"%3Cinit%3E()"},{"p":"org.example","c":"Heap","l":"heapsort(int[])"},{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
