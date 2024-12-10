9555798c8de46f302e8ea723640cab62338376f2
ttomilov <t.tomilov@g.nsu.ru>
2024-12-10 09:41:55 +0700

    Task_1_5_1

--- Task_1_5_1/src/main/java/org/main/Main.java
+++ Task_1_5_1/src/main/java/org/main/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Table tableBuilder = new Table.Builder()
                 .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_CENTER, Table.ALIGN_RIGHT)
                 .withRowLimit(8)
-                .withColumns(3)
-                .addRow("Header 1", "Header 2", "Header 3")
+                .withColumns(4)
+                .addRow("Header 1", "Header 2", "Header 3", "Header 4")
                 .addRow("Cell 1", "Cell 2", "Cell 3").build();
         System.out.println(tableBuilder.serialize());
     }
