42c87d509b88b7d6d89e8dad1219cd4bf3e25c53
ttomilov <ttomilov@users.noreply.github.com>
2025-05-02 06:15:00 +0000

    Deploying to gh-pages from @ ttomilov/OOP@f5e478ecc19fe7e3c5f72c8c5bbb00c52e4302c6 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.game"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.game"}];updateSearchResults();
--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.game","l":"BlackJack"},{"p":"org.game","l":"Card"},{"p":"org.game","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.game","l":"BlackJack"},{"p":"org.game","l":"Card"},{"p":"org.game","l":"Main"}];updateSearchResults();
