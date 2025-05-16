c39fb53fae85e4ec34f8390170362dd92e6c1238
ttomilov <t.tomilov@g.nsu.ru>
2025-05-16 17:51:39 +0700

    fix

--- Task_2_1_2/src/main/java/org/main/Client.java
+++ Task_2_1_2/src/main/java/org/main/Client.java
@@ -1,4 +1,66 @@
 package org.main;
 
-public class Client {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client extends Thread {
+    private String host;
+    private int port;
+    private int id;
+    private Logger logger;
+    private ArrayList<Integer> numbers = new ArrayList<>();
+
+    Client(String host, int port, int id) {
+        this.host = host;
+        this.port = port;
+        this.id = id;
+    }
+
+    private boolean notPrimeFinder() {
+        for (int i : numbers) {
+            for (int j = 2; j * j < i; j++){
+                if (i % j == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        try (Socket socket = new Socket(host, port);
+             DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+             DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream())) {
+
+            Logger.log("Client " + id + " connected to server at " + host + ":" + port);
+
+            while (true) {
+                if(socket.isClosed()) {
+                    Logger.log("Server closed connection.");
+                }
+
+                int n;
+                n = dataIn.readInt();
+
+                Logger.log("Client " + id + " is reading...");
+                numbers.clear();
+                for (int i = 0; i < n; i++) {
+                    numbers.add(dataIn.readInt());
+                }
+
+                Logger.log("Client " + id + " started work...");
+                boolean result = notPrimeFinder();
+
+                Logger.log("Client " + id + " finished work!");
+                dataOut.writeInt(result ? 1 : 0);
+                dataOut.flush();
+            }
+
+        } catch (IOException e) {
+            Logger.log("ERROR: " + e.getMessage());
+        }
+    }
+
 }
--- Task_2_1_2/src/main/java/org/main/Server.java
+++ Task_2_1_2/src/main/java/org/main/Server.java
@@ -1,4 +1,46 @@
 package org.main;
 
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Server extends Thread {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private ArrayList<Integer> numbers;
+    private final int port;
+    private Logger logger;
+
+    Server(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String input = null;
+        while (true) {
+            System.out.print("Welcome to the server. Type file name. File must have .bin excitation. Type 'exit' to quit.");
+            input = scanner.nextLine();
+            if (input.equals("exit")) {
+                System.exit(0);
+            }
+
+            File file = new File(Objects.requireNonNull(getClass().getResource((input))).getFile());
+            if (!file.exists()) {
+                System.out.println(input + ": file does not exist.");
+                continue;
+            }
+
+            try {
+                DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
