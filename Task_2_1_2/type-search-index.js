2b5806932d3a9e575399a5221502864392948826
ttomilov <ttomilov@users.noreply.github.com>
2025-05-22 05:53:21 +0000

    Deploying to gh-pages from @ ttomilov/OOP@6da9c821c8b025fe7f23aed0dfdc78ac5b46fa0a 🚀

---
+++ Task_2_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.main","l":"Client"},{"p":"org.main","l":"Logger"},{"p":"org.main","l":"Main"},{"p":"org.main","l":"Server"}];updateSearchResults();
